1fddca123dcfb493d5905fe9b8caa78c10d064d4
Devanshu <itsdevanshu@hotmail.com>
2022-02-20 20:52:49 +0530

    Pig Game with dynamic winning score

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, winningScore;
 
 init();
 
@@ -47,7 +47,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -80,11 +80,20 @@ function nextPlayer() {
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
+document.querySelector('.set-input').addEventListener('click', setWinScore);
+
+function setWinScore(){
+    winningScore=document.querySelector('.set-input').value;
+    document.querySelector('.final-win-score').textContent = 'Winning Score: '+document.querySelector('.set-input').value;
+}
+
+
 function init() {
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
+    winningScore=document.querySelector('.set-input').value;
     
     document.querySelector('.dice').style.display = 'none';
 
@@ -99,24 +108,11 @@ function init() {
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.final-win-score').textContent = 'Winning Score: '+document.querySelector('.set-input').value;
+
+    
+
+
 }
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
 
-
-
-
-
-
-
-
-/*
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-*/
